4063a9dfcbe7831f1890339595b7880f39779ed0
Silas Noeioh <silas1107nevstad@gmail.com>
2016-12-27 14:14:57 -0500

    Initial Commit

---
+++ Poop Scoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poop Scoop
+//
+//  Created by Silas Nevstad on 12/27/16.
+//  Copyright © 2016 Silas Nevstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
